9511b806f1caa2072b2c3805aeebde6aaa6441c2
Rayhan Khan <rayhankhan1226@gmail.com>
2024-07-09 12:39:52 -0400

    Setup simple HTTP response

--- backend/baseapp/urls.py
+++ backend/baseapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path()
+    path('', views.taskList)
 ]
  
--- backend/baseapp/views.py
+++ backend/baseapp/views.py
@@ -1,2 +1,5 @@
 from django.shortcuts import render
+from django.http import response, HttpResponse
 
+def taskList(request):
+    return HttpResponse('To do List')
