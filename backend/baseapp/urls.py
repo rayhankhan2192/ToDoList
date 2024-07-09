c125bac1d670876d9b4000f7e6f5b70a27bae232
Rayhan Khan <rayhankhan1226@gmail.com>
2024-07-10 02:06:03 +0600

    Initial Change

--- backend/baseapp/urls.py
+++ backend/baseapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from . import views
+from . views import TaskList, TaskDetail, TaskCreate, TakeUpdate, DeleteTask
 
 
 urlpatterns = [
-    path('', views.taskList)
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TakeUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', DeleteTask.as_view(), name='task-delete'),
 ]
  
