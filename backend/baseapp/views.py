f7b4b35cc5a91747374db4f8a46976594f644fff
Rayhan Khan <rayhankhan1226@gmail.com>
2024-07-09 15:59:27 -0400

    Added Task List,Create,Update and Delete

--- backend/baseapp/views.py
+++ backend/baseapp/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
-from django.http import response, HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-def taskList(request):
-    return HttpResponse('To do List')
+from .models import Task
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'baseapp/task.html'
+    
+class TaskCreate(CreateView):
+    model = Task
+    #fields = '__all__'
+    fields = [
+        'user',
+        'title',
+        'description',
+        'complete',
+    ]
+    success_url = reverse_lazy('tasks')
+    
+class TakeUpdate(UpdateView):
+    model = Task
+    fields = [
+        'user',
+        'title',
+        'description',
+        'complete',
+    ]
+    success_url = reverse_lazy('tasks')
+
+class DeleteTask(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks') 
+    
