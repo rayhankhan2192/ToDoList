defb10087a3eaba7050a6fbfa3e0a65060801ac4
Rayhan Khan <rayhankhan1226@gmail.com>
2024-07-11 00:36:20 +0600

    initial Changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-d594-%+!(h)-4=hg*d9@f5mk=#*admm6&vk$esk@sqnkojrv(s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.104', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
